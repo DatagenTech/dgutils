c873b11b33ebc140a4002f315213748deff50aab
rlemberskyDatagen <russell.lembersky@datagen.tech>
2022-07-15 15:32:48 -0400

    Human Bounding box. Dont have head keypoints, need to use segmentation to get the max z or y of head. added helper functions to keep the scale of 3d plots.

--- Notebooks/dghelpers.py
+++ Notebooks/dghelpers.py
@@ -173,3 +173,21 @@ def hic_visualize_pose(datapoint_path, skeleton=True):
 	drawing_func(img, right_hand, 2, cmap_idx=4 * nrof_colors // 5)
 
 	return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+def set_axes_radius(ax, origin, radius):
+    x, y, z = origin
+    ax.set_xlim3d([x - radius, x + radius])
+    ax.set_ylim3d([y - radius, y + radius])
+    ax.set_zlim3d([z - radius, z + radius])
+
+
+def set_axes_equal(ax: plt.Axes):
+    limits = np.array([
+        ax.get_xlim3d(),
+        ax.get_ylim3d(),
+        ax.get_zlim3d()])
+
+    origin = np.mean(limits, axis=1)
+    radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
+    set_axes_radius(ax, origin, radius)
